8af20cbf985a71b018b07d80c82e302c95baa4e0
Jimmy Cann <mail@jimmycann.com>
2019-05-05 09:09:53 +1000

    Add SASS support and normalise.css

--- template/gatsby-browser.js
+++ template/gatsby-browser.js
@@ -1,5 +1,7 @@
 // gatsby-browser.js
-require("prismjs/themes/prism-coy.css");
-require("antd/dist/antd.css");
+require('prismjs/themes/prism-coy.css')
+require('antd/dist/antd.css')
+require('normalize.css')
+require('./src/styles/global.sass')
 
 // Optionally add any other gatsby-browser config here
