b870a0defb06a905b5dd432cede51d7b880025f4
Jimmy Cann <mail@jimmycann.com>
2019-05-24 23:08:45 +1000

    Modify init script to fit

--- template/gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-// gatsby-browser.js
-require('prismjs/themes/prism-coy.css')
-require('antd/dist/antd.css')
-require('normalize.css')
-require('./src/styles/global.sass')
-
-// Optionally add any other gatsby-browser config here
