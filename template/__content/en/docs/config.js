97092cbf3eb24246daa698198c56503472708068
Jimmy Cann <mail@jimmycann.com>
2019-06-19 09:17:54 +1000

    Make sure Algolia section is rendered

--- template/__content/en/docs/config.js
+++ template/__content/en/docs/config.js
@@ -1,24 +1,24 @@
 const versionConfig = {
   documentationOrder: [
     {
-      parent: "root",
-      items: ["Introduction", "Why does this exist?", "Convenience Commands"]
+      parent: 'root',
+      items: ['Introduction', 'Why does this exist?', 'Convenience Commands'],
     },
     {
-      parent: "Concepts",
+      parent: 'Concepts',
       items: [
-        "config.js",
-        "Locales",
-        "Versions",
-        "Component Shadowing",
-        "Developer Story"
-      ]
+        'config.js',
+        'Locales',
+        'Versions',
+        'Component Shadowing',
+        'Developer Story',
+      ],
     },
     {
-      parent: "API Reference",
-      items: ["0C Design", "Config", "Content"]
-    }
-  ]
-};
+      parent: 'API Reference',
+      items: ['0C Design', 'Config', 'Content', 'Enabling Algolia'],
+    },
+  ],
+}
 
-module.exports = versionConfig;
+module.exports = versionConfig
